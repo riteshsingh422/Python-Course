0af535b15a5b8e32332f166567476fe194d86902
riteshsingh422 <riteshsingh.cst1@gmail.com>
2025-02-02 21:51:36 +0530

    Create Pro1.py
    First program

---
+++ Basics/Pro1.py
@@ -0,0 +1,28 @@
+print('Hello World')
+print("Ritesh", "Singh") #print in a single line.
+print(35)
+print(35+34) #sum
+
+#Variable
+name = "Ritesh"
+age = 24
+price = 24.25
+
+print(name,age,price)
+print("My name is: ",name)
+print(type(name),type(age),type(price))
+
+#DataTypes
+
+#Integer --> +ve, -ve, 0 (20 -20 0)
+#String --> (Character)
+#Float --> point value (2.0)
+#Boolean --> True or False
+#None --> A=None Means Right now i didnt assign any value to A.
+
+
+#Print Sum of 2 variable
+a= 20
+b=30
+
+print(a+b)
